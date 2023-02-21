c3136797ded96934e65d018c5cc315903aaaeb4d
Raul pallares <rpallares@miners.utep.edu>
2023-02-20 21:10:00 -0700

    fixed terminator method and add tokenizer and copystr methods

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 #include "tokenizer.h"
 
 
@@ -61,21 +61,23 @@ char *word_start(char *str)
 
 
 /* Returns a pointer terminator char following *word */
-
 char *word_terminator(char *word)
-
 {
+  while(*word != '\0' && *(word+1) != '\0')
 
-  while(*word != '\0')
     {
-      if(space_char(*word) == 1)
-	 word++;
+
+      if(space_char(*word))
+
+	return word;
+
       word+=1;
+
     }
-    return word;
+
+  return word;
+
 }
-
-
 
 /* Counts the number of words in the string argument. */
 
@@ -100,10 +102,77 @@ int count_words(char *str)
   return 1 + count_words(last_char + 1 );
 }
 
-char *str_copy(char *instr,short len)
+/* Returns a fresly allocated new zero-terminated string
+
+   containing <len> chars from <inStr> */
+
+char * str_copy(char *instr, short len)
 {
+  char *new_word = malloc(sizeof(char) * len);
+
+  for(int i = 0; i<= len; i++)
+    {
+      *(new_word + i) = *(instr + i);
+    }
+
+  *(new_word + (len +1)) = '\0';
+
+  return new_word;
+}
+
+/* Returns a freshly allocated zero-terminated vector of freshly allocated
+
+   space-separated tokens from zero-terminated str.
+
+   For example, tokenize("hello world string") would result in:
+
+     tokens[0] = "hello"
+
+     tokens[1] = "world"
+
+     tokens[2] = "string"
+
+     tokens[3] = 0
+
+*/
+
+char **tokenize(char* str)
+{
+  int length = count_words(str);
+
+  char **token_vector = malloc(sizeof(char)* length);
   
- 
+  printf("%d\n", length);
+
+  for(int i = 0; i < length; i++)
+    {
+      str = word_start(str) ;
+
+      char *last_letter = word_terminator(str);
+
+      int word_length = last_letter - str;
+
+      *(token_vector + i) = str_copy(str ,word_length);
+
+      printf("%s\n",*(token_vector + i));
+
+      str = last_letter;
+
+    }
+  return token_vector;
+}
 
 
+/* Prints all tokens. */
+
+void print_tokens(char **tokens)
+{
+  while(tokens)
+
+    {
+
+      printf("%s\n", *tokens);
+
+      tokens +=1;
+    }
 }
